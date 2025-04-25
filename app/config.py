ec12d3bdb49a490734690be4884c67055fc02ea0
JamesDM62 <lsufan622000@yahoo.com>
2025-04-24 22:18:07 -0500

    Initial commit for Neuronote

--- app/config.py
+++ app/config.py
@@ -10,5 +10,6 @@ class Config:
     # url in the hidden config vars to start with postgres.
     # so the connection uri must be updated here (for production)
     SQLALCHEMY_DATABASE_URI = os.environ.get(
-        'DATABASE_URL').replace('postgres://', 'postgresql://')
+        'DATABASE_URL', 'sqlite:///dev.db'  # fallback for local development
+    ).replace('postgres://', 'postgresql://')
     SQLALCHEMY_ECHO = True

e2b73bed5d1c6f70c0638401ea749c8a9b3dbce1
JamesDM62 <lsufan622000@yahoo.com>
2025-05-19 07:09:02 -0500

    another fix1

--- app/api/auth_routes.py
+++ app/api/auth_routes.py
@@ -4,7 +4,7 @@ from app.forms import LoginForm
 from app.forms import SignUpForm
 from flask_login import current_user, login_user, logout_user, login_required
 from datetime import timedelta
-from .. import app
+from flask import current_app as app
 
 auth_routes = Blueprint('auth', __name__)
 
